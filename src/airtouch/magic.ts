fa94d4b86e47c66ad92f7cd1d553c12e537816a1
juicydigital-source <juicydigital@gmail.com>
2026-09-25 11:43:11 +1000

    feat: add AirTouch protocol constants

---
+++ src/airtouch/magic.ts
@@ -0,0 +1,107 @@
+
+
+export class MAGIC {
+
+  static HEADER_BYTES = [0x55, 0x55, 0x55, 0xAA];
+
+  static ADDRESS_STANDARD_BYTES = [0x80, 0xb0];
+
+  static ADDRESS_EXTENDED_BYTES = [0x90, 0xb0];
+
+  static MSGTYPE_STANDARD = 0xc0;
+
+  static MSGTYPE_EXTENDED = 0x1f;
+
+  static SUBTYPE_ZONE_CTRL = 0x20;
+
+  static SUBTYPE_ZONE_STAT = 0x21;
+
+  static SUBTYPE_AC_CTRL = 0x22;
+
+  static SUBTYPE_AC_STAT = 0x23;
+
+  static EXT_SUBTYPE_AC_ABILITY = 0x11;
+
+  static EXT_SUBTYPE_AC_ERROR = 0x10;
+
+  static EXT_SUBTYPE_ZONE_NAME = 0x13;
+
+  static LENGTH_AC_ABILITY = 26;
+
+  static AC_POWER_STATES = {
+    KEEP: 0,
+    NEXT: 1,
+    OFF: 2,
+    ON: 3,
+  };
+
+  static AC_MODES = {
+    AUTO: 0,
+    HEAT: 1,
+    DRY: 2,
+    FAN: 3,
+    COOL: 4,
+    KEEP: 5,
+  };
+
+  static AC_FAN_SPEEDS = {
+    AUTO: 0,
+    QUIET: 1,
+    LOW: 2,
+    MEDIUM: 3,
+    HIGH: 4,
+    POWERFUL: 5,
+    TURBO: 6,
+    INTELLIGENT: 8,
+    KEEP: 9,
+  };
+
+  static AC_TARGET_TYPES = {
+    KEEP: 0,
+    SET_VALUE: 4,
+  };
+
+  static AC_UNIT_DEFAULT = 0;
+
+  static AC_TARGET_DEFAULT = 0;
+
+  static ZONE_POWER_STATES = {
+    KEEP: 0,
+    NEXT: 1,
+    OFF: 2,
+    ON: 3,
+    TURBO: 5,
+  };
+
+  static AC_TARGET_STATES = {
+    OFF: 0,
+    HEAT: 1,
+    COOL: 2,
+    AUTO: 3,
+  };
+
+  static ZONE_TARGET_TYPES = {
+    KEEP: 0,
+    DECREMENT: 2,
+    INCREMENT: 3,
+    DAMPER: 4,
+    TEMPERATURE: 5,
+  };
+
+  static ZONE_NUMBER_DEFAULT = 0;
+
+
+  static ATTR_NAME = 'name';
+  static ATTR_ZONE_POWER = 'zone_power_state';
+  static ATTR_ZONE_PERCENTAGE = 'zone_percentage';
+  static ATTR_CURRENT_HEATCOOL = 'zone_current_heatcool';
+  static ATTR_TARGET_HEATCOOL = 'zone_target_heatcool';
+  static ATTR_CURRENT_TEMP = 'zone_current_temp';
+  static ATTR_TARGET_TEMP = 'zone_target_temp';
+  static ATTR_AC_ACTIVE = 'ac_active';
+
+  static ZONE_OR_AC = {
+    ZONE: 'Zone',
+    AC: 'AC',
+  };
+}
